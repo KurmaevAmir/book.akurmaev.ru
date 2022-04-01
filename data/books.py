655b05f8fb0dc64278248376050b8b3f9f3dc411
KurmaevAmir <amirku05@yandex.ru>
2022-04-01 14:35:10 +0300

    Creating main webpage

--- data/books.py
+++ data/books.py
@@ -9,7 +9,7 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 
-from .db_session import SqlAlchemyBase
+from data.db_session import SqlAlchemyBase
 
 
 class Books(SqlAlchemyBase):
@@ -19,9 +19,12 @@ class Books(SqlAlchemyBase):
                            primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    limitation = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     image = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    rating = sqlalchemy.Column(sqlalchemy.Float, default=0.5, nullable=True)
+    reviews = sqlalchemy.Column(sqlalchemy.Integer, default=0, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
+                                     default=datetime.date.today)
     status = sqlalchemy.Column(sqlalchemy.String, default=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
