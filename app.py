0606894f88862b9677a4a4a704f31de2d90c72b1
Adel Salyamov <adelsalyamov@gmail.com>
2022-04-02 17:07:03 +0300

    bugs fixed

--- app.py
+++ app.py
@@ -17,33 +17,42 @@ app.register_blueprint(blueprint, url_prefix="/books")
 def index():
     db_session.global_init("db/users_data.db")
     db_sess = db_session.create_session()
+
     recommendations_list = []
     for book in db_sess.query(Books).filter(Books.rating > 0.9):
         recommendations_list.append((book.content, book.image))
-    novelties_list = []
+
     value_novelties = datetime.date.today() - datetime.timedelta(days=30)
     date = datetime.timedelta(days=30)
+
+    novelties_list = []
     for book in db_sess.query(Books).filter((Books.created_date - value_novelties) <= date):
         novelties_list.append((book.content, book.image, book.created_date))
+
     novelties_list.sort(reverse=True, key=lambda x: x[2])
     novelties_list = novelties_list[:5]
     numbers_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "11+"]
+
     primary_school_list = []
     for book in db_sess.query(Books).filter((Books.limitation == "Начальная школа") |
                                             (Books.limitation.in_(numbers_list[:3]))):
         primary_school_list.append((book.content, book.image))
+
     secondary_school_list = []
     for book in db_sess.query(Books).filter((Books.limitation == "Средняя школа") |
                                             (Books.limitation.in_(numbers_list[4:8]))):
         secondary_school_list.append((book.content, book.image))
+
     high_school_list = []
     for book in db_sess.query(Books).filter((Books.limitation == "Старшая школа") |
                                             (Books.limitation.in_(numbers_list[9:10]))):
         high_school_list.append((book.content, book.image))
+
     students_list = []
     for book in db_sess.query(Books).filter((Books.limitation == "Студентам") |
                                             (Books.limitation == (numbers_list[-1]))):
         students_list.append((book.content, book.image))
+
     return render_template("index.html", recommendations_list=recommendations_list, novelties_list=novelties_list,
                            primary_school_list=primary_school_list, secondary_school_list=secondary_school_list,
                            high_school_list=high_school_list, students_list=students_list)
