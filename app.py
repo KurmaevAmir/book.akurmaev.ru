5126dc34a3884a5487d8a669a5d6e2d817beb60f
KurmaevAmir <amirku05@yandex.ru>
2022-04-11 15:46:08 +0300

    setup REST-API

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from data.first_book import blueprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Z,kjrjTds_secret_key'
-app.register_blueprint(blueprint, url_prefix="/books")
+app.register_blueprint(blueprint, url_prefix="/book")
 
 
 @app.route('/')
@@ -19,7 +19,7 @@ def index():
 
     recommendations_list = []
     for book in db_sess.query(Books).filter(Books.rating > 0.9):
-        recommendations_list.append((book.content, book.image))
+        recommendations_list.append((book.content, book.image, f'book/{book.id}'))
 
     value_novelties = datetime.date.today() - datetime.timedelta(
         days=30)
@@ -29,7 +29,7 @@ def index():
     for book in db_sess.query(Books).filter(
             (Books.created_date - value_novelties) <= date):
         novelties_list.append((book.content, book.image,
-                               book.created_date))
+                               book.created_date, f'book/{book.id}'))
 
     novelties_list.sort(reverse=True, key=lambda x: x[2])
     novelties_list = novelties_list[:5]
@@ -41,28 +41,28 @@ def index():
                                              "Начальная школа") |
                                             (Books.limitation.in_(
                                                 numbers_list[:3]))):
-        primary_school_list.append((book.content, book.image))
+        primary_school_list.append((book.content, book.image, f'book/{book.id}'))
 
     secondary_school_list = []
     for book in db_sess.query(Books).filter((Books.limitation ==
                                              "Средняя школа") |
                                             (Books.limitation.in_(
                                                 numbers_list[4:8]))):
-        secondary_school_list.append((book.content, book.image))
+        secondary_school_list.append((book.content, book.image, f'book/{book.id}'))
 
     high_school_list = []
     for book in db_sess.query(Books).filter((Books.limitation ==
                                              "Старшая школа") |
                                             (Books.limitation.in_(
                                                 numbers_list[9:10]))):
-        high_school_list.append((book.content, book.image))
+        high_school_list.append((book.content, book.image, f'book/{book.id}'))
 
     students_list = []
     for book in db_sess.query(Books).filter((Books.limitation ==
                                              "Студентам") |
                                             (Books.limitation ==
                                              (numbers_list[-1]))):
-        students_list.append((book.content, book.image))
+        students_list.append((book.content, book.image, f'book/{book.id}'))
 
     return render_template("index.html",
                            recommendations_list=recommendations_list,
@@ -74,4 +74,4 @@ def index():
 
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
--- data/first_book.py
+++ data/first_book.py
@@ -1,12 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+
+from data import db_session
+from data.books import Books
 
 blueprint = Blueprint("first_book", __name__,
                       static_folder="static",
                       template_folder="templates")
 
 
-@blueprint.route('/pussinboots')
-def index(title, img_src, book_title, description, cost):
-    return render_template("first_book.html", title=title,
-                           img_src=img_src, book_title=book_title,
-                           description=description, cost=cost)
+@blueprint.route('/<int:id>', methods=['GET'])
+def index(id):
+    db_session.global_init("db/users_data.db")
+    db_sess = db_session.create_session()
+    book = db_sess.query(Books).get(id)
+    if not book:
+        return jsonify({'error': 'Not found'})
+    return render_template("first_book.html", title=book.title,
+                           img_src=book.image, book_title=book.title,
+                           description=book.content)
