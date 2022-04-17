f7ddc39b1f8c454d10bb3208b54b298800ce976c
Adel Salyamov <adelsalyamov@gmail.com>
2022-04-17 16:58:36 +0300

    check authorize

--- data/blueprint_profile.py
+++ data/blueprint_profile.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect
 from data import db_session
+from flask_login import login_required
 
 blueprint_profile = Blueprint("first_book", __name__,
                       static_folder="static",
@@ -7,6 +8,7 @@ blueprint_profile = Blueprint("first_book", __name__,
 
 
 @blueprint_profile.route('/profile')
+@login_required
 def profile():
     db_session.global_init("db/users_data.db")
     db_sess = db_session.create_session()
