82536a27d0e1c902c0463b48ab6f9eb5e14412f4
KurmaevAmir <amirku05@yandex.ru>
2022-04-27 21:15:07 +0300

    Change DB

--- data/users.py
+++ data/users.py
@@ -28,7 +28,8 @@ class User(SqlAlchemyBase, UserMixin):
                                         nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.date.today)
-    shopping_cart = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    shopping_cart = sqlalchemy.Column(sqlalchemy.String, nullable=True,
+                                      default="")
     books = orm.relation("Books", back_populates='user')
 
     def set_password(self, password):
